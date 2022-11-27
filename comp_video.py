26841e3f0b957f762c243627898bd9c5f0d541b3
Atatanoff <atatanoff@ya.ru>
2022-11-27 23:14:33 +0400

    почти доработал

--- comp_video.py
+++ comp_video.py
@@ -1,35 +1,48 @@
-#CVideo 1.01 - прога для создания 3х часового видео из 10секундных кусочков соедененых рандомно с наложением аудиофайла
+#CVideo 1.01 - прога для создания 3х часового видео из кусочков соедененых рандомно с наложением аудиофайла
 
 from moviepy.editor import VideoFileClip, concatenate_videoclips
 
-# ВНИМАНИЕ!!! завтра оформить эти две функции в класс для нарезки кусочков, объекты класса получают аргументы такие как длина кусочков, список файлов
-# ширина файла, фпс
-
-# функция нарезки 10сек кусочков и остатков из видеозаготовок
+#************************************************************************#
+#       VideoCut класс для нарезки кусочков из видеозаготовок            #
+#        self.list_clips - список видеозаготовок                         #
+#        self.width_clip - ширина клипов                                 #
+#        t_end - время конца обрезки видеозаготовки                 #
+#        self.fps = fps - частота кадров                                 #
+#        self.count_clip   - счетчик клипов                         #
+#        t_start - время начала обрезки видеозаготовка          #
+#        self.length_clip = длина клипа                                  #
+##########################################################################        
 
 class VideoCut:
     def __init__(self, clips, width_c=480, length_c=10, fps=24):
         self.list_clips = clips
         self.width_clip = width_c
         self.length_clip = length_c
-        self.fps =       
-        self.cc = 0
-        self.t_start = 0
-        self.t_end = 10
+        self.fps = fps
+        self.count_clip = 0
+        
         for el in clips:
             with VideoFileClip(el) as clip:
-                cc = self.edition_clip(clip, self.t_start, self.t_end, cc)
-    def edition_clip(self, cl: VideoFileClip, t_s, t_e, count_clip):
-        if cl.duration > t_e:
-            cl.subclip(t_s, t_e).write_videofile(f'newclip{count_clip}.mp4')
-            return edition_clip(cl, t_e, t_e+10, count_clip+1)
-        else:
-            cl.subclip(t_s).write_videofile(f'newclip{count_clip}.mp4')
-            return count_clip+1
+                self.edition_clip(clip)
+        print(f"Всего нарезано клипов: {self.count_clip} штук")
     
-
-            
+    def edition_clip(self, cl: VideoFileClip):
+        flag = cl.duration
+        t_start = 0
+        t_end = self.length_clip 
+        while flag > t_end:
+            cl.subclip(t_start, t_end).resize(width = self.width_clip).write_videofile(f'newclip{self.count_clip}.mp4', fps=self.fps)
+            self.count_clip +=1
+            t_start = t_end
+            t_end += self.length_clip
+            flag -= self.length_clip
+            print('\n')
+        if flag > 4:
+            cl.subclip(t_start).resize(width = self.width_clip).write_videofile(f'newclip{self.count_clip}.mp4', fps=self.fps)
+            self.count_clip +=1
+            print('\n')
+                  
 
 if __name__ == '__main__':
-    clips = ('v1.mp4', 'v2.mp4')
-    main(clips)    
+    clips = ('v3.mp4',)
+    VideoCut(clips)
